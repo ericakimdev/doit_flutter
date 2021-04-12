8b7b08b59d809b4e9dfb9d3646a6e8aab32d1af1
Erica <orange7961@gmail.com>
2021-04-12 14:50:07 -0400

    add ch3 codes

---
+++ FirstApp/first_flutter_app/lib/main_ch3_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyApp();
+  }
+}
+
+class _MyApp extends State<MyApp> {
+  var switchValue = false;
+  String test = 'hello'; //text for Button
+  Color _color = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        darkTheme: ThemeData.light(),
+        home: Scaffold(
+            body: Center(
+              child: RaisedButton(
+                child: Text('$test'),
+                color: _color,
+                  onPressed: () {
+                    if (_color == Colors.blue) {
+                      setState(() {
+                        test = 'flutter';
+                        _color = Colors.amber;
+                      });
+                    } else {
+                      setState(() {
+                        test = 'flutter';
+                        _color = Colors.blue;
+                      });
+                    }
+                  }),
+            )));
+  }
+}
+
+
