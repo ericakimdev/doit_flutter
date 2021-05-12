8ae601778e777a4947ebaaad091a4becbb1d623e
Erica <orange7961@gmail.com>
2021-05-11 22:16:09 -0400

    add native example

---
+++ native_example/lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'sendDataExample.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    if (Platform.isIOS) {
+      return CupertinoApp(
+        home: CupertinoNativeApp(),
+      );
+    } else {
+      return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: NativeApp(),
+        //home: SendDataExample(),
+      );
+    }
+  }
+}
+
+class CupertinoNativeApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return null;
+  }
+}
+
+class NativeApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _NativeApp();
+}
+
+class _NativeApp extends State<NativeApp> {
+  static const platform = const MethodChannel('com.flutter.dev/info');
+  String _deviceInfo = 'Unknown info';
+  static const platform3 = const MethodChannel('dev/dialog');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Native API example'),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                _deviceInfo,
+                style: TextStyle(fontSize: 30),
+              ),
+              FlatButton(
+                  onPressed: () {
+                    _showDialog();
+                  },
+                  child: Text('Open Native Window'))
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _getDeviceInfo();
+        },
+        child: Icon(Icons.get_app),
+      ),
+    );
+  }
+
+  Future<void> _getDeviceInfo() async {
+    String deviceInfo;
+    try {
+      final String result = await platform.invokeMethod('getDeviceInfo');
+      deviceInfo = 'Device info : $result';
+    } on PlatformException catch (e) {
+      deviceInfo = "Failed to get Device info: '${e.message}'.";
+    }
+    setState(() {
+      _deviceInfo = deviceInfo;
+    });
+  }
+
+  Future<void> _showDialog() async {
+    try {
+      await platform3.invokeMethod('showDialog');
+    } on PlatformException catch (e) {}
+  }
+
+}
